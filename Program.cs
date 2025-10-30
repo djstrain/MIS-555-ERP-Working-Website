e1510b59b6aca20a75caac00ecae0d75b42025b3
djstrain <djstrain@ksu.edu>
2025-10-30 13:07:34 -0500

    Add MySQL database integration with EF Core
    Configured Entity Framework Core to use a MySQL database by adding connection string to appsettings.json and registering AppDbContents in Program.cs. Ensures database schema is created at startup.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+// connect AppDbContents to database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// UseMySql method specifies that we are using MySQL database
+builder.Services.AddDbContext<AppDbContents>(options =>
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 21)))
+);
 var app = builder.Build();
+
+// Ensure database exists (creates schema if it doesn't)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContents>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
